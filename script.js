05ca52b084f8e9cda9263e3bd351f21c7b2feab9
DarenOwl <Charly.Fox@mail.ru>
2020-03-10 23:59:09 +0500

    OnClick events
    need to be fixed
    x click coordinate doesn't match itself
    y click coordinate is ok

--- script.js
+++ script.js
@@ -1,47 +1,104 @@
+document.addEventListener("DOMContentLoaded", Init);
 
-var context = null;
-var canvas = null;
+var canvas;
+var context;
 var scaleItem = 32;
 
-var score = 0;
-var pause = false;
+var sources = 
+  {
+    0:      './tex/cauldron0.png',
+    1:      './tex/cauldron1.png',
+    2:      './tex/cauldron2.png',
+    3:      './tex/cauldron3.png',
+    4:      './tex/cauldron4.png',
+    5:      './tex/salt.png',
+    6:      './tex/mushrooms.png',
+    7:      './tex/slime.png',
+    8:      './tex/bones.png',
+    9:      './tex/warn.png',
+    circle: './tex/circle.png',
+  };
 
-function loadImages(sources, callback)
+var images = {};
+
+function Init() {
+    //canvas
+	  canvas = document.getElementById('myCanvas');
+    context = canvas.getContext("2d");
+
+    //размер canvas
+    var size = scaleItem * 6;
+    var pxScale = canvas.offsetWidth / size;
+    context.canvas.width  = size;
+    context.canvas.height = size;
+
+    //шрифты
+    context.font = "10px Arial";
+    context.fillStyle = "#84ffff";
+    context.textAlign = "center";
+
+    //при клике мышью
+    canvas.onclick = function(e){
+      var x = (e.clientX - canvas.offsetLeft);
+      var y = (e.clientY - canvas.offsetTop);
+
+      ClickAt(x,y, pxScale);
+    };
+
+    //при клике с телефона
+    canvas.ontouchstart= function(e){
+      var x = (e.clientX - canvas.offsetLeft);
+      var y = (e.clientY - canvas.offsetTop);
+
+      ClickAt(x,y, pxScale);
+    };
+    
+    //картинки
+    for(var name in sources)
+    {
+      images[name] = new Image();
+      images[name].src = sources[name];
+    }
+}
+
+function ClickAt(x,y, scale)
 {
-    var images = {};
-    var loadedImages = 0;
-    var numImages = 0;
-    // get num of sources
-    for(var src in sources)
+    console.log("clicked at point" + x + ";" + y + " (px size)")
+    console.log("clicked at point" + (x / scale) + ";" + (y / scale) + " (inside size)")
+
+    if (scaleItem * scale < y && y < scaleItem * 2 * scale) //строка, где находятся ингридиенты
     {
-      numImages++;
-    }
-    for(var src in sources)
-    {
-      images[src] = new Image();
-      images[src].onload = function()
-      {
-        if(++loadedImages >= numImages)
-        {
-          callback(images);
+        for(var n = 0; n < 5; n++)
+        {   
+            //столбец ингридиента с индексом n
+            if ((n * scaleItem* 1.25 * scale) < x && x < ((n * scaleItem * 1.25) + scaleItem) * scale)
+            {
+              IngredientClicked(n);
+              return;
+            }
         }
-      };
-      images[src].src = sources[src];
-    }
-  }
+    }    
+}
+
+function IngredientClicked(n)
+{
+    console.log("clicked at ingredient " + n)
+    context.drawImage(images.circle, n * (scaleItem * 1.25), scaleItem, scaleItem, scaleItem);
+}
 
   var sources = 
   {
-    0:    './tex/cauldron0.png',
-    1:    './tex/cauldron1.png',
-    2:    './tex/cauldron2.png',
-    3:    './tex/cauldron3.png',
-    4:    './tex/cauldron4.png',
-    5:    './tex/salt.png',
-    6:    './tex/mushrooms.png',
-    7:    './tex/slime.png',
-    8:    './tex/bones.png',
-    9:    './tex/warn.png',    
+    0:      './tex/cauldron0.png',
+    1:      './tex/cauldron1.png',
+    2:      './tex/cauldron2.png',
+    3:      './tex/cauldron3.png',
+    4:      './tex/cauldron4.png',
+    5:      './tex/salt.png',
+    6:      './tex/mushrooms.png',
+    7:      './tex/slime.png',
+    8:      './tex/bones.png',
+    9:      './tex/warn.png',
+    circle: './tex/circle.png',
   };
 
   function getRandomInt(max)
@@ -54,11 +111,11 @@ document.addEventListener('keydown', function(event)
 {
     if(event.code == "KeyP")
     {
-        pause = !pause;
+        playerTurn = !playerTurn;
     }
   });
 
-
+//CAULDRON
 var cauldronX;
 var cauldronY;
 
@@ -66,44 +123,27 @@ var cauldronFrame = 0;
 
 document.addEventListener("DOMContentLoaded", function ()
 {
-    canvas = document.getElementById('myCanvas');
-    context = canvas.getContext("2d");
-
-    context.canvas.width = scaleItem * 6;
-    context.canvas.height = scaleItem * 6;
-    context.font = "20px Arial";
-    context.fillStyle = "#84ffff";
-    context.textAlign = "center";
 
     cauldronX = context.canvas.width/2 - scaleItem;
     cauldronY = context.canvas.height/2;
 
 
     setInterval(function()
-    {
-        if(pause)
+    {        
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        //отрисовка котла
+        if (cauldronFrame == 5)
         {
-            return;
+            cauldronFrame = 0;
+        }            
+        context.drawImage(images[cauldronFrame], cauldronX, cauldronY, scaleItem*2, scaleItem*2);
+        cauldronFrame++;
+
+        //отрисовка ингридиентов
+        for(var n = 5; n < 10; n++)
+        {   
+            context.drawImage(images[n], (n-5) * (scaleItem* 1.25), scaleItem, scaleItem, scaleItem);
         }
-
-        loadImages(sources, function(images)
-        {
-
-            context.clearRect(0, 0, canvas.width, canvas.height);
-
-            //draw Cauldron
-            if (cauldronFrame == 5)
-            {
-                cauldronFrame = 0;
-            }            
-            context.drawImage(images[cauldronFrame], cauldronX, cauldronY, scaleItem*2, scaleItem*2);
-            cauldronFrame++;
-
-            //draw Ingredients
-            for(var n = 5; n < 10; n++)
-            {   
-                context.drawImage(images[n], (n-5) * (scaleItem* 1.25), scaleItem, scaleItem, scaleItem);
-            }
-        });
     }, 100);
 });
