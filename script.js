ec5afa3501ea9afc3c1c1f1c01dc24d7a80df367
DarenOwl <Charly.Fox@mail.ru>
2020-03-12 00:08:33 +0500

    flying  ingredients
    an ingredient flies from a plate to the cauldron on the plate click

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ var sources =
     7:      './tex/slime.png',
     8:      './tex/bones.png',
     9:      './tex/warn.png',
+    10:      './tex/one_salt.png',
+    11:      './tex/one_mushroom.png',
+    12:      './tex/one_slime.png',
+    13:      './tex/one_bone.png',
+    14:      './tex/one_warn.png',    
     circle: './tex/circle.png',
   };
 
@@ -37,6 +42,7 @@ function Init() {
     context.fillStyle = "#84ffff";
     context.textAlign = "center";
 
+    //определение координат курсора
     function getMousePos(canvas, evt) {
       var rect = canvas.getBoundingClientRect();
       return {
@@ -49,14 +55,6 @@ function Init() {
     canvas.onclick = function(e)
     {
       ClickAt(getMousePos(canvas, e).x, getMousePos(canvas, e).y, pxScale);
-    };
-
-    //при клике с телефона
-    canvas.ontouchstart= function(e){
-      var x = (e.clientX - canvas.offsetLeft);
-      var y = (e.clientY - canvas.offsetTop);
-
-      ClickAt(x,y, pxScale);
     };
     
     //картинки
@@ -86,46 +84,70 @@ function ClickAt(x,y, scale)
     }    
 }
 
+//CAULDRON
+var cauldronX;
+var cauldronY;
+var cauldronFrame = 0;
+
+var flyingIngredients = [];
+
+//про вылетающие из тарелок ингридиенты
+function AnimateFlyingIngredient(){
+    var continueFlying = [];
+
+    var count = flyingIngredients.length;
+
+    for(var i = 0; i < count; i++){
+      // выбираем ингридиент
+      var ing = flyingIngredients.pop();
+
+      //проверяем, что он ещё не в котле и рисуем
+      if (ing.y < cauldronY + scaleItem/2)
+        {
+          //перемещаем
+          ing.x += ing.direction.x;
+          ing.y += ing.direction.y;
+          //рисуем
+          context.drawImage(images[ing.id], ing.x, ing.y, scaleItem/2, scaleItem/2);
+          continueFlying.push(ing);
+
+          console.log("flies number " + ing.id)
+        }
+    }
+
+    count = continueFlying.length;
+    for(var i = 0; i < count; i++){
+      flyingIngredients.push(continueFlying.pop());
+    }
+};
+
 function IngredientClicked(n)
 {
     console.log("clicked at ingredient " + n)
     context.drawImage(images.circle, n * (scaleItem * 1.25), scaleItem, scaleItem, scaleItem);
+
+    var direct = getDirection(n * 1.25 * scaleItem, scaleItem, cauldronX+ scaleItem/2, cauldronY)
+  
+    flyingIngredients.push({id: n+10,
+                            x: n * scaleItem* 1.25,
+                            y: scaleItem*2,
+                            direction: direct
+                          });
 }
 
-  var sources = 
-  {
-    0:      './tex/cauldron0.png',
-    1:      './tex/cauldron1.png',
-    2:      './tex/cauldron2.png',
-    3:      './tex/cauldron3.png',
-    4:      './tex/cauldron4.png',
-    5:      './tex/salt.png',
-    6:      './tex/mushrooms.png',
-    7:      './tex/slime.png',
-    8:      './tex/bones.png',
-    9:      './tex/warn.png',
-    circle: './tex/circle.png',
-  };
-
-  function getRandomInt(max)
-  {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
-
-document.addEventListener('keydown', function(event)
+function getRandomInt(max)
 {
-    if(event.code == "KeyP")
-    {
-        playerTurn = !playerTurn;
-    }
-  });
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
-//CAULDRON
-var cauldronX;
-var cauldronY;
-
-var cauldronFrame = 0;
+//чтобы находить направление к котлу
+function getDirection(startX, startY, destX, destY)
+{
+  return {
+    x: (destX - startX) / 10,
+    y: (destY - startY) / 10
+  };
+}
 
 document.addEventListener("DOMContentLoaded", function ()
 {
@@ -151,5 +173,8 @@ document.addEventListener("DOMContentLoaded", function ()
         {   
             context.drawImage(images[n], (n-5) * (scaleItem* 1.25), scaleItem, scaleItem, scaleItem);
         }
+
+        //отрисовка ингридиентов, летящих в котел
+        AnimateFlyingIngredient();
     }, 100);
 });
